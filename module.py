1915c9eeb45fc3bcc3ee281fec0956fce9e7df58
Yu Xiang <yuxiangyang1105@gmail.com>
2026-01-13 23:42:02 +0100

    commit check

--- module.py
+++ module.py
@@ -4,7 +4,9 @@ class Net(nn.Module):
     def __init__(self):
         super().__init__()
         self.l1 = nn.Linear(1,2)
+        self.l2 = nn.Linear(2,1)
 
     def forward(self, x):
         x = self.l1(x)
+        x = self.l2(x)
         return x
